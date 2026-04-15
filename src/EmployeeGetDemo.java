efe50ae7ac10875806c0d7cf3aa17ee16c4968ad
Hajar <hajar2000alfarsi@gmail.com>
2026-04-15 19:34:11 +0400

    Task Task 2: Retrieve and Display Values from a Java HashMap

---
+++ src/EmployeeGetDemo.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmployeeGetDemo {
+    public static void main(String[] args){
+        HashMap<Integer,String> employees=new HashMap<>(); //create HashMap
+        //Add employees id and name
+        employees.put(101,"Ali");
+        employees.put(102, "Sara");
+        employees.put(103, "Omar");
+        employees.put(104, "Noor");
+        employees.put(105, "Maha");
+
+        //Display all employees details
+        for(Map.Entry<Integer,String> x:employees.entrySet()){
+            System.out.println(x.getKey()+ " --> " + x.getValue());
+        }
+
+        System.out.println("Employees with ID 101 "+employees.get(101));
+        System.out.println("Employees with ID 103 "+employees.get(103));
+    }
+}
